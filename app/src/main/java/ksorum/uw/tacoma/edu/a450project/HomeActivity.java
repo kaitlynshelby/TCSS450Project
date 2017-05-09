6782db1d97c64257b99f6393ab1a71d42e04746a
Kaitlyn Kinerk <ksorum@uw.edu>
2017-05-09 16:51:51 -0700

    Added functionality to add a user to the User database. Removed name requirement for signup (users will be id'd by email), and added password confirmation.

--- app/src/main/java/ksorum/uw/tacoma/edu/a450project/HomeActivity.java
+++ app/src/main/java/ksorum/uw/tacoma/edu/a450project/HomeActivity.java
@@ -1,16 +1,28 @@
 package ksorum.uw.tacoma.edu.a450project;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HomeActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener,
-SignUpFragment.OnFragmentInteractionListener,
-MainFragment.OnFragmentInteractionListener {
+        SignUpFragment.OnAddUser,
+        MainFragment.OnFragmentInteractionListener {
 
     private SectionsAdapter mSectionsAdapter;
     private ViewPager mViewPager;
@@ -55,9 +67,19 @@ MainFragment.OnFragmentInteractionListener {
     }
 
     @Override
+    public void addUser(String url) {
+        AddUserTask task = new AddUserTask();
+        task.execute(new String[]{url.toString()});
+
+        // Takes you back to the previous fragment by popping the current fragment out.
+        getSupportFragmentManager().popBackStackImmediate();
+    }
+
+    @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
+
 
 
  /*   private void setupViewPager(ViewPager viewPager) {
@@ -67,5 +89,76 @@ MainFragment.OnFragmentInteractionListener {
         viewPager.setAdapter(adapter);
 
     }*/
+
+    private class AddUserTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to add user, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+
+
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            try {
+                Log.d("HomeActivity", result);
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+                if (status.equals("success")) {
+                    Toast.makeText(getApplicationContext(), "User successfully added!"
+                            , Toast.LENGTH_LONG)
+                            .show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to add: "
+                                    + jsonObject.get("error")
+                            , Toast.LENGTH_LONG)
+                            .show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Something wrong with the data" +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
 }
 
