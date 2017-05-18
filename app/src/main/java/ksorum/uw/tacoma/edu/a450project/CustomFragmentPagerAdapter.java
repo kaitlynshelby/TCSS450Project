1822a068c3631f91f86b6a4352d7bfb5c8c1b3c5
Jasmine Dacones <jasminedacones@gmail.com>
2017-05-17 22:38:14 -0700

    Bare bones implementation of tab layout provided in a test tab Activity. Will integrate into LandingPageActivity once fully complete.

---
+++ app/src/main/java/ksorum/uw/tacoma/edu/a450project/CustomFragmentPagerAdapter.java
@@ -0,0 +1,33 @@
+package ksorum.uw.tacoma.edu.a450project;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class CustomFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 2;
+    private String tabTitles[] = new String[] { "Inventory", "Shopping List" };
+    private Context context;
+
+    public CustomFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return InventoryFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
