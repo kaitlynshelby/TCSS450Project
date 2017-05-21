eb89353f5e623f3c9b50254c3bcce0206fbf3dca
Jasmine Dacones <jasminedacones@gmail.com>
2017-05-21 12:15:08 -0700

    Working on delete item portion.

--- app/src/main/java/ksorum/uw/tacoma/edu/a450project/TestTabActivity.java
+++ app/src/main/java/ksorum/uw/tacoma/edu/a450project/TestTabActivity.java
@@ -1,5 +1,9 @@
 package ksorum.uw.tacoma.edu.a450project;
 
+/*
+https://guides.codepath.com/android/Google-Play-Style-Tabs-using-TabLayout
+ */
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +11,7 @@ import android.os.Bundle;
 
 import ksorum.uw.tacoma.edu.a450project.inventoryitem.InventoryItem;
 
-public class TestTabActivity extends AppCompatActivity implements InventoryFragment.OnListFragmentInteractionListener {
+public class TestTabActivity extends AppCompatActivity implements InventoryFragment.OnListFragmentInteractionListener, InventoryItemDetailsFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,27 @@ public class TestTabActivity extends AppCompatActivity implements InventoryFragm
         tabLayout.setupWithViewPager(viewPager);
     }
 
+    /**
+     * Launches the item details fragment for the item
+     * that is clicked on.
+     *
+     * @param item the inventory item that is clicked on.
+     */
     @Override
     public void onListFragmentInteraction(InventoryItem item) {
+        InventoryItemDetailsFragment detailsFragment = new InventoryItemDetailsFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(InventoryItemDetailsFragment.INVENTORY_ITEM_SELECTED, item);
+        detailsFragment.setArguments(args);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container_2, detailsFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
