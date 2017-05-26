bd1bd7882ee93fd6d84b431f2c537fda32ea9ecb
Jasmine Dacones <jasminedacones@gmail.com>
2017-05-26 16:24:51 -0700

    Implemented color-coding system for the expiration dates.

--- app/src/main/java/ksorum/uw/tacoma/edu/a450project/inventory/MyInventoryRecyclerViewAdapter.java
+++ app/src/main/java/ksorum/uw/tacoma/edu/a450project/inventory/MyInventoryRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package ksorum.uw.tacoma.edu.a450project.inventory;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.media.Image;
 import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -21,6 +23,7 @@ import org.json.JSONObject;
 import ksorum.uw.tacoma.edu.a450project.R;
 import ksorum.uw.tacoma.edu.a450project.inventory.InventoryFragment.OnListFragmentInteractionListener;
 import ksorum.uw.tacoma.edu.a450project.inventory.inventoryitem.InventoryItem;
+import ksorum.uw.tacoma.edu.a450project.shoppinglist.MyShoppingListRecyclerViewAdapter;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -28,9 +31,16 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static java.security.AccessController.getContext;
+import static java.util.concurrent.TimeUnit.DAYS;
 import static ksorum.uw.tacoma.edu.a450project.R.drawable.delete_button;
 import static ksorum.uw.tacoma.edu.a450project.R.drawable.waste_bin;
 
@@ -40,12 +50,14 @@ import static ksorum.uw.tacoma.edu.a450project.R.drawable.waste_bin;
  */
 public class MyInventoryRecyclerViewAdapter extends RecyclerView.Adapter<MyInventoryRecyclerViewAdapter.ViewHolder> {
 
-    /** The list of inventory items */
+    /**
+     * The list of inventory items
+     */
     private final List<InventoryItem> mValues;
-    /** Fragment listener in the list */
+    /**
+     * Fragment listener in the list
+     */
     private final OnListFragmentInteractionListener mListener;
-
-    private final OnDeleteItem mDeleteListener;
 
     private static final String URL =
             "http://cssgate.insttech.washington.edu/~ksorum/deleteInventoryItem.php?";
@@ -56,20 +68,14 @@ public class MyInventoryRecyclerViewAdapter extends RecyclerView.Adapter<MyInven
 
     /**
      * Adapter constructor.
-     * @param items the list of inventory items
+     *
+     * @param items    the list of inventory items
      * @param listener the fragment listener for the list
      */
     public MyInventoryRecyclerViewAdapter(Context context, List<InventoryItem> items, OnListFragmentInteractionListener listener) {
         mValues = items;
         mListener = listener;
         mContext = context;
-
-        if (context instanceof OnDeleteItem) {
-            mDeleteListener = (OnDeleteItem) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnDeleteListener");
-        }
     }
 
     @Override
@@ -100,17 +106,84 @@ public class MyInventoryRecyclerViewAdapter extends RecyclerView.Adapter<MyInven
 
             @Override
             public void onClick(View v) {
-                boolean deleted = mDeleteListener.deleteItem(holder.mItem.getItemName(),
-                        holder.mItem.getQuantity(), holder.mItem.getPrice());
-
-                if (deleted) {
-                    mValues.remove(pos);
-                    notifyItemRemoved(pos);
-                    notifyItemRangeChanged(pos, mValues.size());
-                }
+                mValues.remove(pos);
+                notifyItemRemoved(pos);
+                notifyItemRangeChanged(pos, mValues.size());
+                Log.i("DELETE", "Click working");
+                String url = buildURL(holder.mItem.getItemName());
+                DeleteItemTask task = new DeleteItemTask();
+                task.execute(new String[]{url});
 
             }
         });
+
+
+        String itemExpiration = mValues.get(pos).getExpiration();
+        System.out.println("Item's Expiration Date: " + itemExpiration);
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Date itemDate;
+        Date compareDate;
+
+        // Today's date
+        Date todaysDate = new Date();
+        DateFormat todaysDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String today = todaysDateFormat.format(todaysDate);
+
+        // TODO: Remove date, only used for testing purposes
+        String date2 = "2017-06-13";
+
+        try {
+            itemDate = df.parse(itemExpiration);
+            compareDate = df.parse(date2);
+            String newDateString = df.format(itemDate);
+            String compareNewDateString = df.format(compareDate);
+            Log.i("CHECK", "Date parsed and formatted");
+
+            long diff = Math.round((itemDate.getTime() - compareDate.getTime()) / (double) 86400000);
+
+            if (diff < 0) {
+                diff *= -1;
+            } else {
+                diff *= 1;
+            }
+            int difference = (int) diff;
+            System.out.println("Days difference: " + difference);
+
+            if (difference <= 1) {
+                holder.mView.setBackgroundColor(Color.RED);
+                holder.mView.getBackground().setAlpha(100);
+            } else if (difference >= 2 && difference <= 3) {
+                holder.mView.setBackgroundColor(Color.YELLOW);
+                holder.mView.getBackground().setAlpha(100);
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String buildURL(String name) {
+        StringBuilder sb = new StringBuilder(URL);
+
+        try {
+
+            mSharedPreferences = mContext.getSharedPreferences
+                    (mContext.getString(R.string.LOGIN_PREFS), Context.MODE_PRIVATE);
+
+            sb.append("name=");
+            sb.append(URLEncoder.encode(name, "UTF-8"));
+
+            String user = mSharedPreferences.getString("user", "");
+            sb.append("&user=");
+            sb.append(URLEncoder.encode(user, "UTF-8"));
+
+            Log.i("InventoryFragment", sb.toString());
+
+        } catch (Exception e) {
+
+        }
+        return sb.toString();
     }
 
 
@@ -138,9 +211,73 @@ public class MyInventoryRecyclerViewAdapter extends RecyclerView.Adapter<MyInven
         }
     }
 
+    /**
+     * Launches AsyncTask to execute the web service to delete an item
+     * from the inventory.
+     */
+    private class DeleteItemTask extends AsyncTask<String, Void, String> {
 
-    public interface OnDeleteItem {
-        boolean deleteItem(String name, String quantity, String price);
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    java.net.URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to delete course, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+                if (status.equals("success")) {
+                    Toast.makeText(mContext, "Item successfully deleted"
+                            , Toast.LENGTH_LONG)
+                            .show();
+                } else {
+                    Toast.makeText(mContext, "Failed to delete: "
+                                    + jsonObject.get("error")
+                            , Toast.LENGTH_LONG)
+                            .show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(mContext, "Something wrong with the data" +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 }
